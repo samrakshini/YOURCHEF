895a1f7b8933890bd026511a43fc31d2ac01862e
samrakshini <samrakshini@gmail.com>
2018-03-03 20:05:08 +0530

    added dashboard fragment to the code

--- app/src/main/java/com/example/android/yourchef/reg_as_chef.java
+++ app/src/main/java/com/example/android/yourchef/reg_as_chef.java
@@ -1,5 +1,8 @@
 package com.example.android.yourchef;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +11,37 @@ import android.view.MenuItem;
 
 public class reg_as_chef extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_as_chef);
 
+        mDrawerLayout = findViewById(R.id.chdrawer);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        FragmentManager fragmentManager=getSupportFragmentManager();
+                        FragmentTransaction
+                                fragmentTransaction=fragmentManager.beginTransaction();
+                        Dashboard f1=new Dashboard();
+                        fragmentTransaction.add(R.id.dashboard,f1);
+                        fragmentTransaction.commit();
+
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
 }
