966a653ba886941de9f85e2b1a1e5d68a4c71dab
samrakshini <samrakshini@gmail.com>
2018-04-06 11:22:34 +0530

    Added code to insert data into firebase

--- app/src/main/java/com/example/android/yourchef/Register_here.java
+++ app/src/main/java/com/example/android/yourchef/Register_here.java
@@ -1,13 +1,20 @@
 package com.example.android.yourchef;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,6 +25,7 @@ public class Register_here extends AppCompatActivity {
     EditText username,mobile_no,password,full_name,email;
     private DatabaseReference databaseReference;
     FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +40,35 @@ public class Register_here extends AppCompatActivity {
    email=(EditText)findViewById(R.id.email_id1);
    firebaseDatabase=FirebaseDatabase.getInstance();
    databaseReference=firebaseDatabase.getReference("Users");
-
+    mAuth=FirebaseAuth.getInstance();
 
     }
     public void chef_reg(View view)
     {
+
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register_here.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
         User_chef user_chef=new User_chef(username.getText().toString(),mobile_no.getText().toString(),password.getText().toString(),full_name.getText().toString(),email.getText().toString());
-        databaseReference.child(user_chef.username).setValue(user_chef);
+        databaseReference.child().setValue(user_chef);
+
         Toast.makeText(Register_here.this,"Registered successfully as chef",Toast.LENGTH_SHORT);
         final Intent ch = new Intent(this,Chef_pane.class);
         startActivity(ch);
